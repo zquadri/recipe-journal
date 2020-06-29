dce242dc505dafc52241edcaf07d4795dd15014c
Zaid Quadri <zquadri98@gmail.com>
2020-06-28 23:03:55 -0400

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
